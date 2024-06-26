aefb0b6743a385ddcecb3ce820a20b8d4228f098
Jevin Vekaria <jevin925@gmail.com>
2024-06-27 00:22:22 +0530

    port change

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT + '...');
